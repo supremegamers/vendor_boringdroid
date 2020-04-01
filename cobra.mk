011f3dd68733a82b01e47c5b6bc14ea2796b71fc
utzcoz <utzcoz@outlook.com>
2020-04-01 17:19:24 +0800

    Move product about config to cobra.mk
    Signed-off-by: utzcoz <utzcoz@outlook.com>

---
+++ cobra.mk
@@ -0,0 +1,15 @@
+
+PRODUCT_REMOVE_PACKAGES := \
+    QuickSearchBox \
+    Music \
+    Browser2 \
+    HTMLViewer \
+    Dialer \
+    Contacts \
+
+# Recents
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.recents.grid=true
+
+PRODUCT_PACKAGES := \
+    CobraSystemUI
