5ed2df4147a549ed6375041af6ced41e9228f663
utzcoz <utzcoz@outlook.com>
2020-04-01 18:59:23 +0800

    Move freeform config to vendor/cobra
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- cobra.mk
+++ cobra.mk
@@ -13,3 +13,9 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 PRODUCT_PACKAGES := \
     CobraSystemUI
+
+PRODUCT_COPY_FILES := \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:system/etc/permissions/android.software.freeform_window_management.xml
+
+PRODUCT_PACKAGE_OVERLAYS := \
+    vendor/cobra/overlay
