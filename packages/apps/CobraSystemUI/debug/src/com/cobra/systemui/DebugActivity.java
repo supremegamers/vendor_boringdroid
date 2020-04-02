74d5de14ed88eca3844a0eddfbb5e27bcc8560e5
utzcoz <utzcoz@outlook.com>
2020-04-02 22:17:06 +0800

    Add build.gradle for CobraSystemUI
    Signed-off-by: utzcoz <utzcoz@outlook.com>

---
+++ packages/apps/CobraSystemUI/debug/src/com/cobra/systemui/DebugActivity.java
@@ -0,0 +1,6 @@
+package com.cobra.systemui;
+
+import android.app.Activity;
+
+public class DebugActivity extends Activity {
+}
--- packages/apps/CobraSystemUI/src/com/cobra/systemui/SystemUIOverlay.java
+++ packages/apps/CobraSystemUI/src/com/cobra/systemui/SystemUIOverlay.java
@@ -1,15 +1,28 @@
 package com.cobra.systemui;
 
 import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.android.systemui.plugins.OverlayPlugin;
 import com.android.systemui.plugins.annotations.Requires;
 
 @Requires(target = OverlayPlugin.class, version = OverlayPlugin.VERSION)
 public class SystemUIOverlay implements OverlayPlugin {
+    private static final String TAG = "SystemUIOverlay";
+    private Context mPluginContext;
+    private View mNavBar;
+    private View mBtAllApps;
+
     @Override
     public void setup(View statusBar, View navBar) {
+        Log.e(TAG, "setup status bar " + statusBar + ", nav bar " + navBar);
+        mNavBar = navBar;
+        if (navBar instanceof ViewGroup) {
+            ((ViewGroup) navBar).addView(mBtAllApps);
+        }
     }
 
     @Override
@@ -24,11 +37,25 @@ public class SystemUIOverlay implements OverlayPlugin {
 
     @Override
     public void onCreate(Context sysUIContext, Context pluginContext) {
-
+        mPluginContext = pluginContext;
+        mBtAllApps = initializeAllAppsButton(mPluginContext, mBtAllApps);
     }
 
     @Override
     public void onDestroy() {
+        if (mNavBar instanceof ViewGroup) {
+            ((ViewGroup) mNavBar).removeView(mBtAllApps);
+        }
+    }
 
+    private View initializeAllAppsButton(Context context, View btAllApps) {
+        if (btAllApps != null) {
+            return btAllApps;
+        }
+        btAllApps =
+                LayoutInflater
+                        .from(context)
+                        .inflate(R.layout.layout_all_apps, null);
+        return btAllApps;
     }
 }
