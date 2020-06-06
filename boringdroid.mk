32ee097a442aa9bfbd19a6aa4a1522b760812f53
utzcoz <utzcoz@outlook.com>
2020-06-07 00:56:07 +0800

    Device should assign boringdorid overlay in its makefile
    It will give device permission to assign boringdroid overlay with the
    specific priority.
    
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- boringdroid.mk
+++ boringdroid.mk
@@ -8,5 +8,3 @@ PRODUCT_PACKAGES := \
 PRODUCT_COPY_FILES := \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:system/etc/permissions/android.software.freeform_window_management.xml
 
-PRODUCT_PACKAGE_OVERLAYS := \
-    vendor/boringdroid/overlay
