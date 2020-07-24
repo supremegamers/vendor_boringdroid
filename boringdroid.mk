dfda4434635a77afa7caa00ea023b3a7a4d5a65c
utzcoz <utzcoz@outlook.com>
2020-07-24 22:02:11 +0800

    Enable persist.sys.pcmode.enabled
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- boringdroid.mk
+++ boringdroid.mk
@@ -1,6 +1,7 @@
 # Recents
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.recents.grid=true \
+    persist.sys.pcmode.enabled=true \
     persist.sys.systemuiplugin.enabled=true
 
 PRODUCT_PACKAGES := \
