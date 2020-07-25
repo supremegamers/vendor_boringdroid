5af6c417c23c08cc3dd5247a048b7fcf66558b94
utzcoz <utzcoz@outlook.com>
2020-07-25 16:41:06 +0800

    Add default listening tuner keys
    Use -- as delimiter.
    
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- boringdroid.mk
+++ boringdroid.mk
@@ -2,7 +2,8 @@
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.recents.grid=true \
     persist.sys.pcmode.enabled=true \
-    persist.sys.systemuiplugin.enabled=true
+    persist.sys.systemuiplugin.enabled=true \
+    persist.sys.settings.tunerkeys=sysui_nav_bar--sysui_nav_bar_left--sysui_nav_bar_right\
 
 PRODUCT_PACKAGES := \
     BoringdroidSystemUI
