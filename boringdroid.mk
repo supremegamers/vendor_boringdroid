0e2218bf7ca02deed47f18e33032e89d14cad60f
utzcoz <utzcoz@outlook.com>
2021-03-18 17:04:42 +0800

    Use prebuilt apk module to replace source code module
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- boringdroid.mk
+++ boringdroid.mk
@@ -5,8 +5,8 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.sys.systemuiplugin.enabled=true
 
 PRODUCT_PACKAGES := \
-    BoringdroidSystemUI \
-    BoringdroidSettings
+    BoringdroidSystemUIApk \
+    BoringdroidSettingsApk \
 
 PRODUCT_COPY_FILES := \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:system/etc/permissions/android.software.freeform_window_management.xml
